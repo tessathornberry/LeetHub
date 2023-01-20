d92494722b44d0b777aced99e76395c6a00c9198
Tessa Thornberry <tthornberryclass@gmail.com>
2023-01-19 19:18:23 -0800

    Time: 88 ms (76.09%), Space: 43.5 MB (71.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,11 +7,32 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) {
         return false;
     }
-    var string1 = t.split('').sort().join('');
-    var string2 = s.split('').sort().join('');
-    if (string1 === string2) {
-         return true;
-    } else {
-return false;}
+//     var string1 = t.split('').sort().join('');
+//     var string2 = s.split('').sort().join('');
+//     if (string1 === string2) {
+//          return true;
+//     } else {
+// return false;}
     
+    var object1 = {};
+    var object2 = {};
+    for (var i = 0; i < s.length; i++) {
+        if (!object1[s[i]]) {
+             object1[s[i]] = 1;
+        } else {
+            object1[s[i]]++;
+        }
+               if (!object2[t[i]]) {
+             object2[t[i]] = 1;
+        } else {
+            object2[t[i]]++;
+        }
+    }
+    var anagram = true;
+  for (var key in object1) {
+      if (object1[key] !== object2[key]) {
+          anagram = false;
+      }
+  }
+    return anagram;
 };
