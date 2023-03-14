4c0cffc153b94e661b1b1134baef6d2a37c4b5f4
Tessa Thornberry <tthornberryclass@gmail.com>
2023-03-14 15:49:10 -0700

    Time: 93 ms (63.24%), Space: 50.5 MB (46.88%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} n
+ * @return {number}
+ */
+var minPartitions = function(n) {
+    let splitNumber = n.split('');
+    let maxNum = 0;
+    for (var i = 0; i < splitNumber.length; i++) {
+        if (Number(splitNumber[i]) > maxNum) {
+            maxNum = splitNumber[i];
+            }
+    }
+    return maxNum;
+};
