809b2797f31768919d325c2145cfebf3b4ec1d61
Tessa Thornberry <tthornberryclass@gmail.com>
2023-03-28 19:21:11 -0700

    Time: 206 ms (16.00%), Space: 51.3 MB (48.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  if (x < 0) {
-     return false;
-  }
+//   if (x < 0) {
+//      return false;
+//   }
   
   let numberString = x.toString();
   if (numberString === numberString.split('').reverse().join('')) {
