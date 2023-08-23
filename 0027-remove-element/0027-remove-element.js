139dabc818635698360c3a48057689ecaee95dee
Tessa Thornberry <tthornberryclass@gmail.com>
2023-08-23 15:35:20 -0700

    Time: 48 ms (87.40%), Space: 42.4 MB (15.76%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  var newArray = [];
+  for (var i = 0; i < nums.length;) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+
+  return nums.length;
+};
