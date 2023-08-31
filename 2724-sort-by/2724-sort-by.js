b0354075508c5390956013cfbeda6f1d412e3bdf
Tessa Thornberry <tthornberryclass@gmail.com>
2023-08-31 16:32:20 -0700

    Time: 117 ms (95.07%), Space: 56.3 MB (63.23%) - LeetHub

---
+++ 2724-sort-by/2724-sort-by.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+
+    function swap(a, b) {
+        return (fn(a) < fn(b)) ? -1 : 1
+    }
+
+    return arr.sort(swap)
+};
