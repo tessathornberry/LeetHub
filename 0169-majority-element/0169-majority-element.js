b8ecae8d3a316f767d729553eeaead7916d684e3
Tessa Thornberry <tthornberryclass@gmail.com>
2023-09-08 15:51:57 -0700

    Time: 54 ms (84.54%), Space: 45.8 MB (18.99%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,11 +4,9 @@
  */
 var majorityElement = function(nums) {
   nums = nums.sort();
-
     if (nums.length % 2 === 0) {
         return nums[nums.length/2];
     } else {
-        return nums[(Math.round(nums.length/2 -1))];
-      
+        return nums[(Math.round(nums.length/2 - 1))];
     }
 };
