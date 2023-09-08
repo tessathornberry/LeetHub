eb75dc402a8f95dafb103baacf6270b29556caf2
Tessa Thornberry <tthornberryclass@gmail.com>
2023-09-08 09:51:20 -0700

    Time: 121 ms (22.34%), Space: 42.6 MB (52.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,11 +6,7 @@
 var twoSum = function(nums, target) {
   var resultArray= [];
   var firstIndex = 0;
-  //I want the first item to be added to each of the following items
-  //if they pair up to the target, then
-  // push the indices into the result array
-  //return the result array
-  //if they do not add up, move over one and compare those
+  
   while (firstIndex < nums.length && resultArray.length === 0) {
     for (var i = firstIndex + 1; i < nums.length; i++) {
       if (nums[firstIndex] + nums[i] === target) {
