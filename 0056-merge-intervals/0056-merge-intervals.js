213f197ff3fb0d8c583d1b143d4feee42bee9067
Tessa Thornberry <tthornberryclass@gmail.com>
2023-09-11 19:54:59 -0700

    Time: 131 ms (11.26%), Space: 52.1 MB (9.07%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+  var newArray = [];
+  function compareNumbers(a, b) {
+  return a[0] - b[0];
+}
+  intervals.sort(compareNumbers);
+  console.log('intervals', intervals)
+  if (intervals.length === 1) {
+    return intervals;
+  }
+  var first = 0;
+  var second = 0;
+  
+  for (var i = 1; i < intervals.length; i++) {
+      
+    if (intervals[i][0] > intervals[i-1][1]) {
+      continue;
+    } 
+    if (intervals[i][0] <= intervals[i-1][1]) {
+      intervals[i-1][0] = Math.min(intervals[i][0], intervals[i-1][0]);
+      intervals[i-1][1] = Math.max(intervals[i][1], intervals[i-1][1]);
+      intervals.splice(i, 1);
+      i = 0;
+    }
+    
+  
+    
+   
+  }
+  
+  return intervals;
+};
